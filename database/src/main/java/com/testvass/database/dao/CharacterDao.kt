f19c6f2fc5db250d9d5a872b4174202f595cf65b
Deimer Villa Pedraza <deymer.villa@petroprix.com>
2023-12-15 16:46:10 -0500

    The DAO class for the Character entity was created.

---
+++ database/src/main/java/com/testvass/database/dao/CharacterDao.kt
@@ -0,0 +1,33 @@
+package com.testvass.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.testvass.database.constants.DatabaseConstants.Columns.ID
+import com.testvass.database.constants.DatabaseConstants.Tables.CHARACTER_TABLE
+import com.testvass.database.entities.CharacterEntity
+
+@Dao
+interface CharacterDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(character: CharacterEntity)
+
+    @Update
+    suspend fun update(character: CharacterEntity)
+
+    @Delete
+    suspend fun delete(character: CharacterEntity)
+
+    @Query("DELETE FROM $CHARACTER_TABLE")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM $CHARACTER_TABLE")
+    suspend fun fetchAll(): List<CharacterEntity>
+
+    @Query("SELECT * FROM $CHARACTER_TABLE WHERE $ID = :characterId")
+    suspend fun fetchByCharacterId(characterId: Int): CharacterEntity
+}
