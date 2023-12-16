69adb57123789c667aa4523ccee9d7b1d5008ec4
Deimer Villa Pedraza <deymer.villa@petroprix.com>
2023-12-16 14:13:45 -0500

    The Retrofit libraries were implemented, the base URL to be used in the application was configured, and the file for module constants was added.

---
+++ network/src/main/java/com/testvass/network/constants/NetworkConstants.kt
@@ -0,0 +1,15 @@
+package com.testvass.network.constants
+
+object NetworkConstants {
+
+    object DEFAULTS {
+        const val DEFAULT_TIMEOUT = 10L
+        const val DEFAULT_LIMIT = 20
+    }
+
+    object URLs {
+        const val CHARACTER_PATH = "character"
+        const val LOCATION_PATH = "location"
+        const val EPISODE_PATH = "episode"
+    }
+}
