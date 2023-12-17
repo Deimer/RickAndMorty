bc90f29f0bd4054264ba17c9c8b185b41f2a19a9
Deimer Villa Pedraza <deymer.villa@petroprix.com>
2023-12-16 19:52:55 -0500

    New character model for data

---
+++ repository/src/main/java/com/testvass/repository/models/CharacterModel.kt
@@ -0,0 +1,17 @@
+package com.testvass.repository.models
+
+data class CharacterModel(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val species: String,
+    val type: String,
+    val gender: String,
+    val origin: String,
+    val location: String,
+    val image: String,
+    val episodes: List<String>,
+    val url: String,
+    val created: String,
+    var isFavorite: Boolean
+)
