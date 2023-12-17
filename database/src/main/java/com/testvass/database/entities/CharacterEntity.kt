09b9c2d2f695ea53f89a1e7a93108c6b577a1fc0
Deimer Villa Pedraza <deymer.villa@petroprix.com>
2023-12-16 21:46:54 -0500

    New character mapper file

--- database/src/main/java/com/testvass/database/entities/CharacterEntity.kt
+++ database/src/main/java/com/testvass/database/entities/CharacterEntity.kt
@@ -25,11 +25,11 @@ data class CharacterEntity(
     @ColumnInfo(name = Columns.ORIGIN_NAME)
     val originName: String,
     @ColumnInfo(name = Columns.ORIGIN_URL)
-    val originUrl: String,
+    val originUrl: String = "",
     @ColumnInfo(name = Columns.LOCATION_NAME)
     val locationName: String,
     @ColumnInfo(name = Columns.LOCATION_URL)
-    val locationUrl: String,
+    val locationUrl: String = "",
     @ColumnInfo(name = Columns.IMAGE)
     val image: String,
     @ColumnInfo(name = Columns.EPISODES)
@@ -37,5 +37,7 @@ data class CharacterEntity(
     @ColumnInfo(name = Columns.URL)
     val url: String,
     @ColumnInfo(name = Columns.CREATED)
-    val created: String
+    val created: String,
+    @ColumnInfo(name = Columns.IS_FAVORITE)
+    val isFavorite: Boolean
 )
